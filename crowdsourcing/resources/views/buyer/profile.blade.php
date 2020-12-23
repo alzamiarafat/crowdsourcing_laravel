4455717abf0961e56b5bd7d74996fee1dbe96fe3
zami <alzamiarafat00@gmail.com>
2020-12-24 02:12:33 +0600

    Preview Profile Picture

--- crowdsourcing/resources/views/buyer/profile.blade.php
+++ crowdsourcing/resources/views/buyer/profile.blade.php
@@ -43,14 +43,41 @@
 
 			<tr>
 				<td></td>
-				<td></td>
+				<td><a href="edit_profile/<%=profile.username %>"><button type="button" class="btn btn-secondary">Edit Profile</button></a><br><br></td>
 			</tr>
 
 		</table>
+
+		
+
+		<form method="POST" enctype="multipart/form-data">
+			<div style="position: absolute; left: 30%; top: 120px">
+
+				<img id="previewImg" src="" width="200" height="200"><br>
+				<input type="file" name="profile_photo" accept="image/*" onchange="document.getElementById('previewImg').src = window.URL.createObjectURL(this.files[0])"><br><br>
+				<button>Upload</button>
+
+			</div>
+				
+		</form>
+		
 	</fieldset>
 	</div>
-@endsection
 
-@section('title')
-Home Page
+	<script>
+		 var loadFile = function (input) {
+			var previewImg = document.getElementById('previewImg');
+			previewImg.src = URL.createObjectURL(input.target.files[0]);
+			//alert('ok')
+			/*var file = $("input[type=file]").get(0).files[0];
+
+			if (file) {
+				var reader =new FileReader();
+				reader.onload = function() {
+					$('#previewImg').attr("src",reader.result);
+				}
+				reader.readAsDataURL(file);
+			}*/
+		}
+	</script>
 @endsection
