5be9b8f9b045288e75d4d3468fb2a36176cc20da
zami <alzamiarafat00@gmail.com>
2020-12-24 02:42:00 +0600

    db migration

--- crowdsourcing/resources/views/buyer/profile.blade.php
+++ crowdsourcing/resources/views/buyer/profile.blade.php
@@ -53,8 +53,10 @@
 		<form method="POST" enctype="multipart/form-data">
 			<div style="position: absolute; left: 30%; top: 120px">
 
-				<img id="previewImg" src="" width="200" height="200"><br>
-				<input type="file" name="profile_photo" accept="image/*" onchange="document.getElementById('previewImg').src = window.URL.createObjectURL(this.files[0])"><br><br>
+				<img id="previewImg" src="" width="200" height="200"><br><br>
+				<input type="file" style="border: 1px solid lightgray;border-radius: 5px;" name="profile_photo" accept="image/*" 		onchange="document.getElementById('previewImg').src = window.URL.createObjectURL(this.files[0])">
+				<br><br>
+				
 				<button>Upload</button>
 
 			</div>
@@ -64,20 +66,4 @@
 	</fieldset>
 	</div>
 
-	<script>
-		 var loadFile = function (input) {
-			var previewImg = document.getElementById('previewImg');
-			previewImg.src = URL.createObjectURL(input.target.files[0]);
-			//alert('ok')
-			/*var file = $("input[type=file]").get(0).files[0];
-
-			if (file) {
-				var reader =new FileReader();
-				reader.onload = function() {
-					$('#previewImg').attr("src",reader.result);
-				}
-				reader.readAsDataURL(file);
-			}*/
-		}
-	</script>
 @endsection
--- crowdsourcing/resources/views/layout/mainLayout.blade.php
+++ crowdsourcing/resources/views/layout/mainLayout.blade.php
@@ -20,9 +20,12 @@
 	<a href="{{route('logout',csrf_token())}}">Log Out</a> 
 	
 	@yield('content')
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
-	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
+
 
 </body>
 </html>
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js" 
+	integrity="sha512-AA1Bzp5Q0K1KanKKmvN/4d3IRKVlv9PYgwFPvm32nPO6QS8yH1HO7LbgB1pgiOxPtfeg5zEn2ba64MUcqJx6CA==" crossorigin="anonymous">
+</script>
