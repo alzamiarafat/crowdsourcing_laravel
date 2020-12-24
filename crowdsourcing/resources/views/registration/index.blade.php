3cda4f8339867470d5f0259e5dbc70c778b48d51
Saqib <saqib29111@gmail.com>
2020-12-25 00:39:57 +0600

    add Registration page with design layout

--- crowdsourcing/resources/views/registration/index.blade.php
+++ crowdsourcing/resources/views/registration/index.blade.php
@@ -1,59 +1,72 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Registration</title>
-</head>
-<body>
-	<form method="POST">
-		<input type="hidden" name="_token" value="{{csrf_token()}}">
-		<fieldset>
-			<legend>Registration</legend>
-			
-			<table>
-			<tr>
-				<td>Full Name</td>
-				<td><input type="text" name="full_name" required/></td>
-			</tr>
-			<tr>
-				<td>Username</td>
-				<td><input type="text" name="username" required/></td>
-			</tr>
-			<tr>
-				<td>Password</td>
-				<td><input type="password" name="password" required/></td>
-			</tr>
-			<tr>
-				<td>Retype Password</td>
-				<td><input type="password" name="repassword" required/></td>
-			</tr>
-			<tr>
-				<td>Email</td>
-				<td><input type="email" name="email"/></td>
-			</tr>
-			<tr>
-				<td>Contacts</td>
-				<td><input type="text" name="contact"/></td>
-			</tr>
-			<tr>
-				<td>Address</td>
-				<td><input type="text" name="address"/></td>
-			</tr>
-			<tr>
+@extends('layout.commonlayout')
 
-				<td>Register as</td>
+@section('content')
+<div class="jumbotron vartical-center content-center">
+	<div class="container">
+		<div class="row-md-6">
+			<div class="content-center">
+				<form method="POST">
+					
+				<h1>Registration</h1>
+				<br><br><br>
+				
+					<table>
+						<input type="hidden" name="_token" value="{{csrf_token()}}">
 
-				<td> <input type="text" name="user_roll" placeholder="Buyer/Seller" required></td>
+						<tr>
+							
+						<td>Full Name</td>  
+						<td><input type="text" name="full_name" placeholder="Full Name" required/><br><br></td>
+					
+						</tr>
+						<tr>
+							<td>Username</td>  
+							<td><input type="text" name="username" placeholder="Username" required/><br><br></td>
+						</tr>
+					
+						<tr>
+							<td>Password </td>
+							<td><input type="password" name="password" placeholder="Password" required/><br><br></td>
+						</tr>
+						<tr>
+							<td>Retype Password</td> 
+							<td><input type="password" name="repassword" placeholder="Retype-password" required/><br><br></td>
+						</tr>
+					
+						<tr>
+							<td>Email</td> 
+							<td><input type="email" name="email" placeholder="Email"/><br><br></td>
+						</tr>
+					
+						<tr>
+							<td>Contacts </td>
+							<td><input type="text" name="contact" placeholder="Contact"/><br><br></td>
+						</tr>
+					
+						<tr>
+							<td>Address</td>
+							<td><input type="text" name="address" placeholder="Address"/><br><br></td>
+						</tr>
+				
+						<tr>
+							<td>Register as</td> 
+							<td><input type="text" name="user_roll" placeholder="Buyer/Seller" required><br><br></td>	
+						</tr>
+						
+						<tr>
+							<td></td>
+							<td style="float: right"><input type="submit" name="submit" value="Submit"></td>
+						</tr>
+						
+					</table>
+					<br><br>
+					<p class="message">Already registered? <a href="/login">Sign In</a></p>
+				
+				</form>
+			</div>
+		</div>
+	</div>
+</div>
+@endsection
+	
 
-			</tr>
-			<tr>
-				<td></td>
-				<td><input type="submit" name="submit" value="Submit"></td>
-			</tr>
-		</table>
-		<p class="message">Already registered? <a href="/login">Sign In</a></p>
-		
-		</fieldset>
-	</form>
-
-</body>
-</html>
